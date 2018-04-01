56b72e4f3f4d24720cb88adae975ebad84647d54
Hattan Shobokshi <hattan@gmail.com>
2018-04-01 14:16:55 -0700

    initial commit

---
+++ ToDos/ToDos.Client/Services/AppState.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Blazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ToDos.Shared;
+
+namespace ToDos.Client.Services
+{
+    public class AppState
+    {
+        public List<Todo> Todos { get; private set; }
+
+        public event Action OnChange;
+
+        private readonly HttpClient http;
+        public AppState(HttpClient httpInstance)
+        {
+            http = httpInstance;
+        }
+
+        public async Task FetchTodos()
+        {
+            Todos = await http.GetJsonAsync<List<Todo>>("/api/todo/index");
+            NotifyStateChanged();
+        }
+
+        public async Task AddTodo(string name)
+        {
+            var todo = new Todo { Name = name };
+            todo = await http.PostJsonAsync<Todo>("/api/todo/save",todo);
+            Todos.Add(todo);
+            NotifyStateChanged();
+        }
+
+        public async Task ClearTodo(int todoId)
+        {
+            var todo = Todos.Where(i => i.Id == todoId).FirstOrDefault();
+            if(todo!=null)
+            {
+                Todos.Remove(todo);
+                NotifyStateChanged();
+            }
+
+            await http.PutJsonAsync<Todo>("/api/todo/clear", todoId);
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
---
+++ ToDos/ToDos.Server/Controllers/TodoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDos.Shared;
+
+namespace ToDos.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        public static int counter = 0;
+        public static int getNextId()
+        {
+            return ++counter;
+        }
+        public static List<Todo> Data { get; set; } = new List<Todo>()
+            {
+                 new Todo{Id=getNextId(),Name="test 123",IsComplete=false}
+            };
+
+        [HttpGet("[action]")]
+        public IEnumerable<Todo> Index()
+        {
+            return Data.Where(i => !i.IsComplete ); 
+        }
+
+        [HttpPost("[action]")]
+        public Todo Save([FromBody] Todo todo)
+        {
+            todo.Id = getNextId();
+            Data.Add(todo);
+            return todo;
+        }
+
+        [HttpPut("[action]")]
+        public void Clear([FromBody] int id)
+        {
+            var todo = Data.Where(i => i.Id == id).FirstOrDefault();
+            if (todo != null)
+            {
+                todo.IsComplete = true;
+            }
+        }
+    }
+}
