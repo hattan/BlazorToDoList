201d41dd2229333840898a5626b25309f1e1037e
Hattan Shobokshi <hattan@gmail.com>
2018-04-01 14:38:48 -0700

    Added test project

---
+++ Todos.Test/Server/ToDoControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ToDos.Server.Controllers;
+using Xunit;
+
+namespace Todos.Test.Server
+{
+    public class ToDoControllerTests
+    {
+        [Fact]
+        public void Foo()
+        {
+            //arrange
+            var controller = new TodoController();
+
+            //act
+            var todos = controller.Index();
+
+            //assert
+            Assert.Single(todos);
+            Assert.Equal("test 123", todos.FirstOrDefault()?.Name);
+
+        }
+    }
+}
